335f7cac67a285bd1f35b2d750db41474d17c257
Samyajit <samyajitd@gmail.com>
2026-04-26 15:35:02 +0530

    misc

--- cortex/__init__.py
+++ cortex/__init__.py
@@ -2,11 +2,26 @@
 
 from .budget import BudgetTracker
 from .deliberator import CortexDeliberator, DeliberationLog
+from .llm import HuggingFaceProvider, LLMRole
 from .memory import EpisodeMemory
+from .roles import (
+    CriticRole,
+    ExecutiveRole,
+    PerceptionRole,
+    PlannerRole,
+    WorldModelerRole,
+)
 
 __all__ = [
     "BudgetTracker",
     "CortexDeliberator",
+    "CriticRole",
     "DeliberationLog",
     "EpisodeMemory",
+    "ExecutiveRole",
+    "HuggingFaceProvider",
+    "LLMRole",
+    "PerceptionRole",
+    "PlannerRole",
+    "WorldModelerRole",
 ]
